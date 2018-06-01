6c27b33305c0b067bc551cb97045bfe77739edf2
Ronny Gutierrez <ronny.icne@gmail.com>
2018-06-01 16:49:04 -0700

    4:48pm

---
+++ js-two/script.js
@@ -0,0 +1,48 @@
+
+
+
+
+$(document).ready(function() {
+
+	
+
+	var typed = new Typed(".typed", {
+		strings: ["Web Developer.", "Designer.", "Passionate Learner."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+	
+});
+
+
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+    
+   
+}
+
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
